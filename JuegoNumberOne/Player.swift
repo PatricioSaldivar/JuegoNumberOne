018ebc44de5456a6fb39c0cd8924fd43e4d244be
PatricioSaldivar <patricio.saldivarf@gmail.com>
2019-02-22 15:47:01 -0600

    Created player class

---
+++ JuegoNumberOne/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  JuegoNumberOne
+//
+//  Created by Pato Saldivar on 2/22/19.
+//  Copyright © 2019 Pato Saldivar. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+ /*
+ 
+ Aqui va a ir la clase del player
+ */
+
