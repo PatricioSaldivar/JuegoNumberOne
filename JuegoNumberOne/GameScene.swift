01078d64b676444385bedba6197753d4d46cc6a8
PatricioSaldivar <patricio.saldivarf@gmail.com>
2019-02-22 15:40:14 -0600

    Skeleton of project, first commit

---
+++ JuegoNumberOne/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  JuegoNumberOne
+//
+//  Created by Pato Saldivar on 2/22/19.
+//  Copyright © 2019 Pato Saldivar. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+}
