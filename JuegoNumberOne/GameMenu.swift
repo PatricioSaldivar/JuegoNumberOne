b379ea4a206ad3d65282f716784ea8c4bd15a5fe
Jorge Elizondo <jorgeelizondo@Jorges-MacBook-Pro-3.local>
2019-02-22 18:14:27 -0600

    Actualizar Player y Game Menu

--- JuegoNumberOne/GameMenu.swift
+++ JuegoNumberOne/GameMenu.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Foundation
+import SpriteKit
+
+
+class GameMenu: SKScene{
+    
+    var startGame = SKLabelNode()
+    var bestScore = SKLabelNode()
+    var gameSettings = Settings.sharedInstance
+    
+    override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        startGame = self.childNode(withName: "startGame") as! SKLabelNode
+        bestScore = self.childNode(withName: "bestScore") as! SKLabelNode
+        bestScore.text = "Best : \(gameSettings.highScore)"
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let touchLocation = touch.location(in: self)
+            if atPoint(touchLocation).name == "startGame"{
+                let gameScene = SKScene(fileNamed: "GameScene")!
+                gameScene.scaleMode = .aspectFill
+                view?.presentScene(gameScene, transition: SKTransition.doorsOpenHorizontal(withDuration: TimeInterval(2)))
+                
+            }
+        }
+        
+    }
+    
+}
--- JuegoNumberOne/Player.swift
+++ JuegoNumberOne/Player.swift
@@ -14,6 +14,36 @@ import SpriteKit
  Aqui va a ir la clase del player
  
  
+ 
  Patricio Saldivar
  */
 
+class Player : SKScene{
+    var tagName:String
+    var player = SKSpriteNode()
+    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let touchLocation = touch.location(in: self)
+            if atPoint(touchLocation).name == "player" {
+                
+                
+            }
+        }
+        
+    }
+    
+    init(tagName: String){
+        self.tagName = tagName
+      
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
